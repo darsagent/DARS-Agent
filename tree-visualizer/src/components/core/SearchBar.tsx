4deab59d0d49d8e771e24f4158900626df41cc29
darsagent <darsagent@gmail.com>
2025-01-31 12:44:22 +0530

    Update SearchBar.tsx

--- tree-visualizer/src/components/core/SearchBar.tsx
+++ tree-visualizer/src/components/core/SearchBar.tsx
@@ -1,7 +1,6 @@
 import SearchIcon from "@mui/icons-material/Search";
 import { styled } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
-import { ChangeEventHandler } from "react";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
