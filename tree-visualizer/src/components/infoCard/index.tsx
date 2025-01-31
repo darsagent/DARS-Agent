1c62cf6236e20a996cca5686562733e088194268
darsagent <darsagent@gmail.com>
2025-01-31 13:29:35 +0530

    Update index.tsx

--- tree-visualizer/src/components/infoCard/index.tsx
+++ tree-visualizer/src/components/infoCard/index.tsx
@@ -2,7 +2,7 @@ import { User } from "./User";
 import { Assistant } from "./Assistant";
 
 //Constants
-import { NodeType } from "@/app/graph/constants";
+import { NodeType } from "@/components/constants";
 
 export const nodeTypes = {
   [NodeType.USER]: User,
